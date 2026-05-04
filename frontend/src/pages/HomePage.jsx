e4e9ab36d5146c9bd526df71f61595def054d080
DELAL <siz@e-posta.com>
2026-05-04 14:14:16 +0300

    guncelleme

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -104,7 +104,7 @@ export default function HomePage() {
                 Herkes Kendi Dergisinin Kapağı Olabilir
               </div>
               <h1 className="hp-h1">
-                Birgün Herkes <br />
+                Bir Gün  <br />
                 <em> Ünlü Olacak</em>
                 <span className="hp-h1-rule" />
                 Andy Warhol              </h1>
